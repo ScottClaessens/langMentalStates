1c75d58ff0108cd188c2691907062d4e1d61a92d
Scott Claessens <scla896@UoA.auckland.ac.nz>
2024-01-22 14:44:00 +1300

    start pipeline

---
+++ R/functions.R
@@ -0,0 +1,37 @@
+# custom functions
+
+# plot proportions of definitions
+plotProportions <- function(d) {
+  # plot
+  out <-
+    d %>%
+    group_by(language) %>%
+    summarise(
+      Overall              = mean(mental_state),
+      `Belief/\nKnowledge` = mean(BK),
+      `Desire/\nWish`      = mean(DW),
+      Intention            = mean(IN),
+      Perception           = mean(PE),
+      Emotion              = mean(EM),
+      Arousal              = mean(AR),
+      Other                = mean(OT)
+    ) %>%
+    pivot_longer(cols = !language) %>%
+    ggplot(aes(x = fct_reorder2(name, language, value), y = value,
+               fill = language)) +
+    geom_col(position = "dodge", colour = "black") +
+    scale_fill_manual(
+      name = "Language",
+      labels = c("English","Tongan"),
+      values = c("gray25","white")
+      ) +
+    labs(
+      x = "Mental state class",
+      y = "Proportion of definitions"
+    ) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+  # save
+  ggsave(out, filename = "plots/proportions.pdf", width = 6, height = 4)
+  return(out)
+}
--- _targets.R
+++ _targets.R
@@ -2,6 +2,16 @@ options(tidyverse.quiet = TRUE)
 library(tarchetypes)
 library(targets)
 library(tidyverse)
+source("R/functions.R")
 
 # full pipeline
-list()
+list(
+  # files
+  tar_target(fileData, "data/dictionaries.rds", format = "file"),
+  # load dictionary data
+  tar_target(d, readRDS(fileData)),
+  # plot proportions of definitions
+  tar_target(plotProp, plotProportions(d)),
+  # session info
+  tar_target(sessionInfo, writeLines(capture.output(sessionInfo()), "sessionInfo.txt"))
+)
